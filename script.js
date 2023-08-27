3f3ba800f1b0bbd214955a81d94bf97621186d9a
Volkan TÜRKMAN <59908974+vturkman@users.noreply.github.com>
2023-08-27 16:52:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".multiple-text", {
+    strings: ["Bilgi Teknolojileri Uzmanı","Bilgi İşlem Uzmanı",
+    "It Specialist","It Manager"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
